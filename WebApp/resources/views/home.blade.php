c673d00b6bd081e53722bd4bea0d787858137f9e
Neil Christian Go <cyberneo12161999@gmail.com>
2021-09-23 13:16:03 +0800

    Added profile image and name with post, follower, and following count
    Updates:
    on home.blade.php
    - Added profile image and name with post, follower, and following count using div and image tags
    
    on app.blade.php
    - Reduced the .svg icon size from 25px to 20 px

--- WebApp/resources/views/home.blade.php
+++ WebApp/resources/views/home.blade.php
@@ -2,22 +2,18 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-4">
+            <img src="https://lh3.googleusercontent.com/a-/AOh14Gg9J9Bs9rBOsUSGgZLCo2JImpmE1-6OC1zZXq7OKw=s576-p-rw-no" class="rounded-circle" style="height: 150px;">
+        </div>
+        <div class="col-9 pt-4">
+            <div><h1>Neil Christian Go</h1></div>
+            <div class="d-flex">
+                <div class="pr-3"><strong>153</strong> posts</div>
+                <div class="pr-3"><strong>23k</strong> followers</div>
+                <div class="pr-3"><strong>153</strong> following</div>
             </div>
         </div>
-    </div>
+    </div>    
 </div>
 @endsection
